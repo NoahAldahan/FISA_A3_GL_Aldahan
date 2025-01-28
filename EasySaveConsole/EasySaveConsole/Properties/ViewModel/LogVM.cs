f3ee33af42d29fede5647952b2e6f82bb65a4ff2
Romain68 <romain.klein.68720@gmail.com>
2025-01-28 14:55:16 +0100

    feat : file creation for log and logvm classes

---
+++ EasySaveConsole/EasySaveConsole/Properties/ViewModel/LogVM.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+
+
+public class Log
+{
+    private string message { get; set; }
+
+    public void WriteLog(string message)
+    {
+        this.message = message;
+    }
+}
