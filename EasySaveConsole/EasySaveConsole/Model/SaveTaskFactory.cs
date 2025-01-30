8c396e7ffe6f96b583c8549c67ebcbff37cbdddf
Noah Aldahan <maxime.aldahan@gmail.com>
2025-01-30 12:16:31 +0100

    feat (logic) : Added a SaveTask Factory to create SaveTasks, modified class diagram

---
+++ EasySaveConsole/EasySaveConsole/Model/SaveTaskFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EasySaveConsole.Model
+{
+    public enum SaveTaskTypes
+    {
+        Differential,
+        Complete
+    }
+    public class SaveTaskFactory
+	{
+        public SaveTask CreateSave(SaveTaskTypes saveTaskTypes, string sourcePath, string targetPath)
+        {
+            switch (saveTaskTypes)
+            {
+                case SaveTaskTypes.Differential:
+                    return new DifferentialSave(DirectoryPair(sourcePath, targetPath));
+                case SaveTaskTypes.Complete:
+                    return new CompleteSave(DirectoryPair(sourcePath, targetPath));
+                default:
+                    throw new ArgumentException("Invalid save task type");
+            }
+        }
+
+	}
+}
