e9da1c01d304fba4957c467d6026db66986a70f6
Yamigiri1 <jean.vogel.g@gmail.com>
2025-02-19 14:46:04 +0100

    feat logic : remove limit save task

---
+++ EasySaveWPFApp/EasySaveWPFApp/Model/ProcessMonitor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Timers;
+
+namespace EasySaveConsole.Utilities
+{
+    public class BusinessSoftwareObserver
+    {
+        private readonly System.Timers.Timer timer;
+        private const string businessSoftware = "cmd.exe"; // Logiciel métier fixe
+        private bool isRunning;
+
+        public event Action<bool> OnBusinessSoftwareStatusChanged;
+
+        public BusinessSoftwareObserver()
+        {
+            timer = new System.Timers.Timer(1000); // Vérification toutes les secondes
+            timer.Elapsed += CheckBusinessSoftware;
+            timer.AutoReset = true;
+            timer.Start();
+        }
+
+        private void CheckBusinessSoftware(object sender, ElapsedEventArgs e)
+        {
+            bool currentlyRunning = Process.GetProcessesByName("cmd").Any();
+
+            if (currentlyRunning != isRunning)
+            {
+                isRunning = currentlyRunning;
+                OnBusinessSoftwareStatusChanged?.Invoke(isRunning);
+            }
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+    }
+}
+
