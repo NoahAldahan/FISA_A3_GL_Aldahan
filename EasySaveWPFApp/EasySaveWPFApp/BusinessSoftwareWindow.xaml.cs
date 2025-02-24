2a8e72b7905ea66cdf3590b09390eda0ef75817d
Yamigiri1 <jean.vogel.g@gmail.com>
2025-02-24 09:20:34 +0100

    feat logic : add Pop-up detection BusinessSoftware

---
+++ EasySaveWPFApp/EasySaveWPFApp/BusinessSoftwareWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using EasySaveWPFApp.Model;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DotNetEnv;
+using EasySaveWPFApp.ViewModel;
+using EasySaveWPFApp.Model;
+using EasySaveWPFApp.Utilities;
+using System.Collections.ObjectModel;
+
+namespace EasySaveWPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class BusinessSoftwareWindow : Window
+    {
+        public BusinessSoftwareWindow()
+        {
+        InitializeComponent(); // Appelle la méthode générée par le XAML
+        }
+
+        private void ClosePopup(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
